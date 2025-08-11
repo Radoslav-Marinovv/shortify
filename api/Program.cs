d5f3a0423408787cc9acf6b2414965c71e68e821
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-11 21:56:12 +0300

    Add CORS for localhost in dev

--- api/Program.cs
+++ api/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowMyLocalhostReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -14,6 +24,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("AllowMyLocalhostReactApp");
 }
 
 app.UseHttpsRedirection();
