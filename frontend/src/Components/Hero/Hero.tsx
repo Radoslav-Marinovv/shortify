287f4a5ed77927c837783da920766297a5639fab
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-11 20:00:53 +0300

    Add GoToButton

--- frontend/src/Components/Hero/Hero.tsx
+++ frontend/src/Components/Hero/Hero.tsx
@@ -1,8 +1,12 @@
+import GoToButton from "../Buttons/GoToButton/GoToButton";
+
 const Hero = () => {
+
   return (
     <>
       <h1>Shortify</h1>
       <p>Your link is too long? Shortify it!</p>
+      <GoToButton location="/create-new-link" displayName="Try it now" />
     </>
   )
 }
