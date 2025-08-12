66b4e5e1a3945cf526d1768b0445acf3b4b5bd36
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-13 00:47:00 +0300

    Add Link model

---
+++ api/Models/Link.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using api.Utils.Constants;
+namespace api.Models
+{
+    public class Link
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [MinLength(Constants.MIN_LENGTH_URL)]
+        public string OriginalURL { get; set; }
+        public string SecretURL { get; set; }
+        public DateTime Created { get; set; }
+        public int TimesOpened { get; set; }
+        public Dictionary<string,int> VisitorsIp { get; set; }
+
+        public Link()
+        {
+            Created = DateTime.UtcNow;
+            TimesOpened = 0;
+            SecretURL = $"{Constants.SECRET_URL_NAME}/{Guid.NewGuid().ToString()}";
+        }
+    }
+}
