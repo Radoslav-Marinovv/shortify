53f051b8874c66072b1286fab194f1a0bd2f9714
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-13 00:45:31 +0300

    Add Constants for minimum url length and secret url path

---
+++ api/Utils/Constants/Constants.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace api.Utils.Constants
+{
+    public static class Constants
+    {
+        public const int MIN_LENGTH_URL = 5;
+        public const string SECRET_URL_NAME = "secret";
+    }
+}
