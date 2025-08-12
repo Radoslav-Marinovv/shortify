15fe99dd4ce44a19a6debf07fb4cf2a371c861e9
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-13 01:55:19 +0300

    Add migration AddLinksTableToDb

---
+++ api/Migrations/20250812224729_AddLinksTableToDb.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLinksTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    OriginalURL = table.Column<string>(type: "TEXT", nullable: false),
+                    SecretURL = table.Column<string>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TimesOpened = table.Column<int>(type: "INTEGER", nullable: false),
+                    VisitorsIpJson = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
