abdf651daed8a2766ce847f0ea4a4403880f56bf
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-13 00:48:42 +0300

    Add ApplicationDbContext and DbSet for Links

---
+++ api/Database/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Link> Links { get; set; }
+    }
+}
