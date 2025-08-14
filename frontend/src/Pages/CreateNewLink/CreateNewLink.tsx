7a7bf4f1f13154ad20f8d5f1cd12f6cc34bc9a22
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-14 08:25:48 +0300

    Enhance CreateNewLink component with suspense loading and display original URL

--- frontend/src/Pages/CreateNewLink/CreateNewLink.tsx
+++ frontend/src/Pages/CreateNewLink/CreateNewLink.tsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 import BackToHome from "../../Components/Buttons/BackToHomeButton/BackToHomeButton";
+import GoToShortUrlAnchor from "../../Components/Buttons/GoToShortUrlAnchor/GoToShortUrlAnchor";
 
-import { postCreateLink } from "../../utils/services/link.service";
-import { MIN_URL_LENGTH } from "../../constants/constants";
+import { postCreateLink, type CreateLinkResponse } from "../../utils/services/link.service";
+import { MIN_URL_LENGTH, SERVER_URL_LOCALHOST_NOT_REAL } from "../../constants/constants";
 import './CreateNewLink.css';
+import GoToSecretUrlAnchor from "../../Components/Buttons/GoToSecretUrlAnchor/GoToSecretUrlAnchor";
 
 const CreateNewLink = () => {
 
   const [baseUrl, setBaseUrl] = useState('');
+  const [createdLink, setCreatedLink] = useState<CreateLinkResponse | null>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -18,40 +21,57 @@ const CreateNewLink = () => {
       return;
     }
 
-    const response = await postCreateLink(baseUrl);
+    const response = await postCreateLink(baseUrl) as CreateLinkResponse;
 
     if (!response) {
       alert(`Failed to create link. Please try again.`);
       return;
     }
+    setCreatedLink(response);
     setBaseUrl('');
   };
 
   return (
     <>
       <div className="container create-new-link">
-        <h1>Create New Link</h1>
-        <form
-          className="create-new-link-form"
-          onSubmit={handleSubmit}
-        >
-          <div className="create-new-link-url-group">
-            <label htmlFor="url">Enter URL:</label>
-            <input
-              type="text"
-              id="url"
-              name="url"
-              placeholder="https://example.com"
-              required
-              value={baseUrl}
-              minLength={MIN_URL_LENGTH}
-              onChange={(e) => setBaseUrl(e.target.value)} />
-          </div>
-          <div className="create-new-link-btn-group">
-            <BackToHome />
-            <button type="submit">Create Link</button>
-          </div>
-        </form>
+        {!createdLink && (
+          <>
+            <h1>Create New Link</h1>
+            <form
+              className="create-new-link-form"
+              onSubmit={handleSubmit}
+            >
+              <div className="create-new-link-url-group">
+                <label htmlFor="url">Enter URL:</label>
+                <input
+                  type="text"
+                  id="url"
+                  name="url"
+                  placeholder="https://example.com"
+                  required
+                  value={baseUrl}
+                  minLength={MIN_URL_LENGTH}
+                  onChange={(e) => setBaseUrl(e.target.value)} />
+              </div>
+              <div className="create-new-link-btn-group">
+                <BackToHome />
+                <button type="submit">Create Link</button>
+              </div>
+            </form>
+          </>)}
+        {createdLink && (
+          <Suspense fallback={<div className="create-new-link">Loading...</div>}>
+            <div>
+              <h1>Link Created Successfully!</h1>
+              <h2>Original URL: {createdLink?.originalURL}</h2>
+              <p>Short URL: <GoToShortUrlAnchor displayName={`${SERVER_URL_LOCALHOST_NOT_REAL}/${createdLink?.shortURL}`} location={`${createdLink?.shortURL}`} /></p>
+              <p>Secret URL: <GoToSecretUrlAnchor displayName={`${SERVER_URL_LOCALHOST_NOT_REAL}/${createdLink?.secretURL}`} location={`${createdLink?.secretURL}`} /></p>
+            </div>
+            <div className="create-new-link-btn-group">
+              <BackToHome />
+            </div>
+          </Suspense>
+        )}
       </div>
     </>
   );
