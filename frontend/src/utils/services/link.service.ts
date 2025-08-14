c87436a8aa467e9d2652fa2ab5541fa661cc55c7
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-14 03:14:54 +0300

    Add getShortUrl, postCreateLink Remove getWeatherForecast Update API_URL

--- frontend/src/utils/services/link.service.ts
+++ frontend/src/utils/services/link.service.ts
@@ -1,23 +1,49 @@
-import type { WeatherForecast } from '../types/link.service.types';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:7096';
-
-export const getWeatherForecast = async () => {
+export const getShortUrl = async (id: string) => {
   try {
-    const response = await fetch(`${API_URL}/WeatherForecast`);
+    const response = await fetch(`${API_URL}/api/Links/${id}`, {
+      method: 'GET',
+    });
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-
-    const data = (await response.json()) as WeatherForecast[];
-    return data;
+    const data = (await response.json()) as { originalUrl: string };
+    return data.originalUrl;
   } catch (error) {
     if (error instanceof Error) {
       console.error('Error fetching data: ', error.message);
     } else {
       console.error('Error fetching data: ', error);
     }
-    return [];
+    return { originalUrl: '' };
+  }
+};
+
+export const postCreateLink = async (originalUrl: string) => {
+  try {
+    const response = await fetch(`${API_URL}/api/Links`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(originalUrl),
+    });
+    if (!response.ok) {
+      console.log(response);
+
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data.id;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error fetching data: ', error.message);
+    } else {
+      console.error('Error fetching data: ', error);
+    }
+    return { id: '' };
   }
 };
