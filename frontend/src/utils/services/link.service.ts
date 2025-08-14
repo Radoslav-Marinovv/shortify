4d98ea046977e300ca43aa7e7608f5865bdd911b
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-14 08:22:23 +0300

    Add getSecretUrl, type CreateLinkResponse Remove console logs

--- frontend/src/utils/services/link.service.ts
+++ frontend/src/utils/services/link.service.ts
@@ -1,9 +1,35 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
+import { SERVER_URL_LOCALHOST_NOT_REAL } from '../../constants/constants';
+
+export type Link = {
+  id: string;
+  originalURL: string;
+  secretURL: string;
+  created: string;
+  timesOpened: number;
+  visitorsIp: { key: string; value: number };
+};
+
+export type CreateLinkResponse = {
+  originalURL: string;
+  shortURL: string;
+  secretURL: string;
+};
+
+export type GetSecretUrl = {
+  originalURL: string;
+  shortURL: string;
+  visitorsIp: { [key: string]: number };
+};
+
+const API_URL = import.meta.env.VITE_API_URL || SERVER_URL_LOCALHOST_NOT_REAL;
 
 export const getShortUrl = async (id: string) => {
   try {
     const response = await fetch(`${API_URL}/api/Links/${id}`, {
       method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
     if (!response.ok) {
@@ -21,6 +47,30 @@ export const getShortUrl = async (id: string) => {
   }
 };
 
+export const getSecretUrl = async (id: string) => {
+  try {
+    const response = await fetch(`${API_URL}/api/Links/Secret/${id}`);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = (await response.json()) as GetSecretUrl;
+
+    return data as GetSecretUrl;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error fetching data: ', error.message);
+    } else {
+      console.error('Error fetching data: ', error);
+    }
+    return {
+      originalURL: 'Error',
+      shortURL: 'Error',
+      visitorsIp: [{ key: 'Error', value: 0 }],
+    };
+  }
+};
+
 export const postCreateLink = async (originalUrl: string) => {
   try {
     const response = await fetch(`${API_URL}/api/Links`, {
@@ -31,13 +81,11 @@ export const postCreateLink = async (originalUrl: string) => {
       body: JSON.stringify(originalUrl),
     });
     if (!response.ok) {
-      console.log(response);
-
       throw new Error('Network response was not ok');
     }
 
     const data = await response.json();
-    return data.id;
+    return data as CreateLinkResponse;
   } catch (error) {
     if (error instanceof Error) {
       console.error('Error fetching data: ', error.message);
