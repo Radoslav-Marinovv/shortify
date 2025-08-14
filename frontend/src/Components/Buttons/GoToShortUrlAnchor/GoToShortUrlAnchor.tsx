2e78ff8e4257e10fdb900e7e281456434ca51738
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-14 07:13:55 +0300

    Add GoToShortUrlAnchor for navigation to the original URL

---
+++ frontend/src/Components/Buttons/GoToShortUrlAnchor/GoToShortUrlAnchor.tsx
@@ -0,0 +1,24 @@
+import { getShortUrl } from "../../../utils/services/link.service";
+
+type AnchorProps = {
+  location: string;
+  displayName: string;
+}
+
+const GoToShortUrlAnchor = ({ location, displayName }: AnchorProps) => {
+
+  const handleCreatedLink = async (location: string) => {
+    if (!location) {
+      alert('No link created yet.');
+      return;
+    }
+    const URL = await getShortUrl(location);
+    window.open(`${URL}`, "_blank")
+  }
+
+  return (
+    <a onClick={() => handleCreatedLink(location)}>{displayName}</a>
+  )
+}
+
+export default GoToShortUrlAnchor;
